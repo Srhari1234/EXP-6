9f84fb75c8dce6ef893b6ac12b31585ce78363b1
Srihari Nair <sriharinair134@gmail.com>
2024-08-02 01:00:24 +0530

    for loop.cpp

--- for loop.cpp
+++ for loop.cpp
@@ -1,5 +1,5 @@
-//Name: Saaj Mulik
-//Prn: 23070123109
+//Name: Srihari Nair
+//Prn: 23070123131
 //Class: EnTC B-2
 
 #include<iostream>
@@ -14,3 +14,22 @@ int main()
     }
     return 0;
 }
+
+/* OUTPUT:
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+
+--------------------------------
+Process exited after 0.0885 seconds with return value 0
+Press any key to continue . . . */
